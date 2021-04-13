27feafa942169d0660fde884351407ea58853140
iigonzoii <a.gonzalez418@gmail.com>
2021-04-12 21:06:25 -0600

    updates

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,36 @@
 const router = require('express').Router();
 
+router.get('/menu', async (req, res) => {
+  res.render('menu', {
+  
+    })});
 
-
-
+   
 router.get('/', async (req, res) => {
 res.render('homepage', {
 
   })});
+
+  // router.get('/', async (req, res) => {
+  //   try {
+  // menuData = await Menu.findAll({
+  //       include: [
+  //         {
+  //           model: Menu,
+  //           attributes: ['name', 'description', 'prices'],
+  //         },
+  //       ],
+  //     });
+  //     const menus = menuData.map((menu) =>
+  //       menu.get({ plain: true })
+  //     );
+  //     res.render('menu', {
+  //       menus,
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //     res.status(500).json(err);
+  //   }
+  // });
 
 module.exports = router; 
